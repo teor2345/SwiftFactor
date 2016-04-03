10d286d2650fd5941463d9b2f35f6308494bf833
teor (Tim Wilson-Brown) <teor2345@gmail.com>
2016-04-04 00:53:38 +1000

    Initial Commit

---
+++ factor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  factor
+//
+//  Created by Tim Wilson-Brown on 4/04/2016.
+//  Copyright © 2016 teor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
