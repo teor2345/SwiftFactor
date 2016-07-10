d6646d1ec3e4d5d5fb3ee6828a6eb5fda2b44997
teor (Tim Wilson-Brown) <teor2345@gmail.com>
2016-07-10 11:09:44 +1000

    Do the GPL thing, exclude xcuserdata, add a README and .gitignore

---
+++ README.md
@@ -0,0 +1,4 @@
+# factor
+
+Factor numbers in Swift using a memoizing prime number generator.
+Written before I learned about the Swift Generator protocol.
--- factor/main.swift
+++ factor/main.swift
@@ -3,7 +3,20 @@
 //  factor
 //
 //  Created by Tim Wilson-Brown on 4/04/2016.
-//  Copyright © 2016 teor. All rights reserved.
+//  Copyright © 2016 teor - gmail: teor2345
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import Foundation
